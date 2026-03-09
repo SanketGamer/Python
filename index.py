1d6ddcc983c50509a31ade39202a717c834ccded
SanketGamer <sanketdas839952@gmail.com>
2026-03-09 11:41:55 +0530

    syntax familiar

--- index.py
+++ index.py
@@ -3,3 +3,25 @@ age = 20
 name = "sanket"
 print(age)
 print(name)
+
+# if else statement
+if age >= 18:
+    print("Adult")
+else:
+    print("minor")
+
+# for loop
+for i in range(4):
+    print(i)
+
+# while loop
+i=0
+while i<5:
+    print(i)
+    i += 1
+
+# define a function
+def greet():
+    print("hello")
+
+greet()
